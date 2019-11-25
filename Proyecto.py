ca25fa014ced15abb05c99001cc1bc3601f0bae5
Mario Palacios <marithojohnson88@gmail.com>
2019-11-25 18:28:13 -0500

    Version 2

--- Proyecto.py
+++ Proyecto.py
@@ -1,7 +1,7 @@
+import os
 menu = [ 'leer' , 'crear' , 'modificar' , 'eliminar' ]
-
+campos_save = ["ID" , "Nombre del cliente" , "Dirección cliente" , "Cantidad de productos" , "Employee" ]
 power = True
-
 print('Bienvenido a FacturaSoft')
 
 while power:
@@ -9,4 +9,16 @@ while power:
     for i in menu:
         print( str(c) + ' -- ' + i + ' factura' )
         c+=1
-    input()
+    entrada = input()
+    entrada = int(entrada)
+    if entrada == 2:
+        data_create = []
+        os.system("cls")
+        for i in campos_save:
+           temp = input(i + ' \n')
+           data_create.append(temp)
+        print(data_create)
+
+
+
+
